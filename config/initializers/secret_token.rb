d2fc19dca9924f2fd9b9f0842137cdcacf2a0a2f
Devika <s.devikanair@gmail.com>
2013-10-25 19:22:08 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HitchedInBackend::Application.config.secret_token = 'f6137d0e295a9dc5ab18d730edacc2c31e52b513f759e4c52c51fc36b29a1ddcac5882d5b38332a18650d2a40b82b9b235d8df51b9afa9c732ce4d20ee8ec5ca'
